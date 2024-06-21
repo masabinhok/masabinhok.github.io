616fa9f79c0d458b0b99cc02387c80def6f1c532
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-21 14:48:28 +0545

    skills new animation

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ import autoText from "./auto-text.js";
 
 heart();
 autoText();
+
+const imageContainerEl = document.querySelector(".image-container");
+let value = 0;
+
+
+setInterval(() => {
+  value -= 40;
+  imageContainerEl.style.transform = `perspective(1000px) rotateY(${value}deg)`;
+},3000);
+
+
